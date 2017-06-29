8619af9245ffdd968d292b5b17da78c82a5f80d6
Jordan Hudgens <jordan@devcamp.com>
2017-06-29 18:05:43 +0000

    Implemented concern for managing image data

--- db/migrate/20170626164651_add_post_status_to_blogs.rb
+++ db/migrate/20170626164651_add_post_status_to_blogs.rb
@@ -1,5 +1,5 @@
-#the previous words right below this message in yellow were:   class AddPostStatusToBlogs < ActiveRecord::Migration[5.0]   I only changed the words:   PostStatus
-class AddStatusEnumToBlogs < ActiveRecord::Migration[5.0]
+
+class AddPostStatusToBlogs < ActiveRecord::Migration[5.0]
   def change
     add_column :blogs, :status, :integer, default: 0 
   end
